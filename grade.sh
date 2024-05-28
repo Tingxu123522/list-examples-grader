c685db06c9f0dab9ebaa3cc0aa57fa1b859d3452
Ting Xu <tingxu@Tings-MacBook-Pro.local>
2024-05-28 09:55:38 -0700

    initial commit

--- grade.sh
+++ grade.sh
@@ -5,12 +5,32 @@ rm -rf grading-area
 
 mkdir grading-area
 
+#clone the student's
 git clone $1 student-submission
 echo 'Finished cloning'
 
 
 # Draw a picture/take notes on the directory structure that's set up after
+if [[ -f student-submission/ListExamples.java ]]; 
+then
+    echo "File exist"
+else
+    echo "ListExamples.java does not exist"
+    echo "Grade: 0"
+    exit 1
+fi
+
 # getting to this point
+cp student-submission/ListExamples.java grading-area
+cp TestListExamples.java grading-area
+cp -r lib grading-area
 
 # Then, add here code to compile and run, and do any post-processing of the
+
+javac -cp $CPATH GradeServer.java Server.java
+
+cd grading-area
+javac $CPATH ListExamples.java TestListExamples.java
+echo "This is the exit code of javac (previous command): $7."
+
 # tests
